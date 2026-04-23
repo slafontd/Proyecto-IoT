3d0a383c4240b89f612d67be15e86311895c83cb
slafontd <slafontd@github.com>
2026-04-23 01:33:21 -0500

    sensor simula multiples sensores

--- sensor.py
+++ sensor.py
@@ -1,36 +1,25 @@
 import socket
 import time
 import random
-import sys
 
-HOST = "127.0.0.1"   # debes configurarlo en hosts
+HOST = "127.0.0.1"
 PORT = 8080
 
 tipos = ["TEMP", "HUM", "PRESS", "VIB", "ENERGY"]
 
-# 🔹 validar argumento
-if len(sys.argv) < 2:
-    print("Uso: python sensor.py <sensor_id>")
-    exit()
-
-sensor_id = sys.argv[1]
-
-# 🔹 cada sensor tiene un tipo fijo
-tipo = tipos[hash(sensor_id) % len(tipos)]
-
-print(f"[INFO] Sensor iniciado: {sensor_id} ({tipo})")
-
 while True:
-    try:
-        valor = random.randint(0, 100)
+    sensor_id = f"sensor{random.randint(1,5)}"
+    tipo = random.choice(tipos)
+    valor = random.randint(0,100)
 
+    try:
         s = socket.socket()
         s.connect((HOST, PORT))
 
         msg = f"DATA {sensor_id} {tipo} {valor}"
         s.send(msg.encode())
 
-        print(f"[SEND] {msg}")
+        print(msg)
 
         s.close()
 
