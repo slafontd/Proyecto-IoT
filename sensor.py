167cacd722bef2454628bfe0338e4011588c4be0
slafontd <slafontd@github.com>
2026-04-22 23:04:46 -0500

    concurrencia de sensores

--- sensor.py
+++ sensor.py
@@ -3,17 +3,20 @@ import socket, time, random
 HOST = "18.222.158.163"
 PORT = 8080
 
-sensor_id = f"sensor{random.randint(1,100)}"
-tipo = random.choice(["TEMP","HUM","PRESS","VIB","ENERGY"])
+tipos = ["TEMP","HUM","PRESS","VIB","ENERGY"]
 
 while True:
+    sensor_id = f"sensor{random.randint(1,10)}"
+    tipo = random.choice(tipos)
+    valor = random.randint(0,100)
+
     s = socket.socket()
     s.connect((HOST, PORT))
 
-    msg = f"DATA {sensor_id} {tipo} {random.randint(0,100)}"
+    msg = f"DATA {sensor_id} {tipo} {valor}"
     s.send(msg.encode())
 
     print(msg)
-    s.close()
 
+    s.close()
     time.sleep(2)
