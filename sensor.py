a9d9305b0092072b7835ea8435f1603b67153862
slafontd <slafontd@github.com>
2026-04-22 22:06:15 -0500

    sensor con conexión constante

--- sensor.py
+++ sensor.py
@@ -2,28 +2,23 @@ import socket
 import time
 import random
 
-HOST = "18.222.158.163"  # luego lo cambiamos a dominio
+HOST = "18.222.158.163"
 PORT = 8080
 
 sensor_id = f"sensor{random.randint(1,100)}"
-
 tipos = ["TEMP", "HUM", "PRESS", "VIB", "ENERGY"]
 tipo = random.choice(tipos)
 
-s = socket.socket()
-s.connect((HOST, PORT))
-
-# REGISTRO
-mensaje = f"REGISTER {sensor_id} {tipo}"
-s.send(mensaje.encode())
-print("Enviado:", mensaje)
-
-time.sleep(1)
-
 while True:
+    s = socket.socket()
+    s.connect((HOST, PORT))
+
     valor = random.randint(0, 100)
     mensaje = f"DATA {sensor_id} {valor}"
+
     s.send(mensaje.encode())
     print("Enviado:", mensaje)
+
+    s.close()
 
     time.sleep(2)
